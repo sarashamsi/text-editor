c1e577c8d5dc5abb38c3ce48401886636e8c1545
sara <sarashmsi834@gmail.com>
2025-04-24 11:53:54 +0330

    add changes on FileManger

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,13 +1,35 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
 
 public class FileManager {
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            textEditor.currentFile = file ;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+                StringBuilder stringBuilder = new StringBuilder();
+                String newLine ;
+                while ((newLine = reader.readLine()) != null){
+                    stringBuilder.append(newLine).append("\n");
+                }
+                textArea.setText(stringBuilder.toString());
+            } catch (IOException e){
+
+                JOptionPane.showMessageDialog(textEditor , "Error --> Cannot open 'File' !"+ e.getMessage());
+            }
+        }
+    }
+
+    public static void writeToFile (File file , String content , TextEditor textEditor){
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))){
+
+            bufferedWriter.write(content);
+
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(textEditor , "Error --> Cannot save 'File' !"+ e.getMessage());
         }
     }
 
@@ -15,15 +37,21 @@ public class FileManager {
         if (textEditor.currentFile == null){
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+
+                File file = fileChooser.getSelectedFile();
+                textEditor.currentFile = file;
+                writeToFile(file , textArea.getText() , textEditor);
             }
         }
         else{
-            //TODO
+            writeToFile(textEditor.currentFile , textArea.getText() , textEditor);
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText(" ");
+        textEditor.currentFile = null ;
     }
+
+
 }
